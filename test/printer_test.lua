441a8e8ecc2dacf7a92ec5549ab7027c7e2abaa4
Masatoshi Fukunaga <mah0x211@gmail.com>
2021-10-23 12:59:04 +0900

    Change the arguments of printer.new function

--- test/printer_test.lua
+++ test/printer_test.lua
@@ -1,10 +1,101 @@
 require('luacov')
-local dump = require('dump')
 local unpack = unpack or table.unpack
-local pcall = pcall
 local assert = require('assertex')
+local printer = require('testcase.printer')
 
-local function test_printer()
+local function test_new()
+    -- test that return new instance of printer
+    local p = printer.new()
+    assert(p, 'new() did not returns new instance of printer')
+
+    -- test that throws error with invalid arguments
+    for _, v in ipairs({
+        {
+            arg = {
+                true,
+            },
+            err = 'test',
+        },
+        {
+            arg = {
+                nil,
+                {},
+            },
+            err = 'test',
+        },
+        {
+            arg = {
+                nil,
+                nil,
+                1,
+            },
+            err = 'test',
+        },
+    }) do
+        local err = assert.throws(function()
+            printer.new(unpack(v.arg))
+        end)
+        assert.match(err, v.err, false)
+    end
+end
+
+local function test_parse_format()
+    -- test that return 0 if an argument is not string
+    for _, v in ipairs({
+        1,
+        true,
+        false,
+        {},
+        function()
+        end,
+    }) do
+        assert.equal(printer.parse_format(), 0)
+    end
+
+    -- test that returns number of params
+    for _, v in ipairs({
+        {
+            fmt = '',
+            nparam = 0,
+        },
+        {
+            fmt = 'foo bar %',
+            nparam = 1,
+        },
+        {
+            fmt = 'foo %s bar %%d baz %q',
+            nparam = 2,
+        },
+    }) do
+        assert.equal(printer.parse_format(v.fmt), v.nparam)
+    end
+end
+
+local function test_vstringify()
+    -- test that returns all arguments as a single string without formatting
+    assert.equal(printer.vstringify(false, 'foo %q baz', 'bar', 1, true, false),
+                 'foo %q bazbar1truefalse')
+
+    -- test that return string from arguments with formatting
+    assert.equal(printer.vstringify(true, 'foo %q baz', 'bar', 1, true, false),
+                 'foo "bar" baz1truefalse')
+
+    -- test that return string from arguments without formatting
+    assert.equal(printer.vstringify(true, 10, 'foo %q baz', 'bar', 1, true,
+                                    false), '10foo %q bazbar1truefalse')
+
+    -- test that throws an error with invalid format string
+    local err = assert.throws(function()
+        printer.vstringify(true, 'foo %', 'bar', 1, true, false)
+    end)
+    assert.match(err, "invalid .+ to 'format'", false)
+end
+
+local function test_call_printline()
+    -- unrequire
+    package.loaded['testcase.printer'] = nil
+    _G['testcase.printer'] = nil
+    -- hook stdout
     local stdout = _G.io.stdout
     local argv
 
@@ -23,86 +114,74 @@ local function test_printer()
             end,
         },
     })
+    local printer = require('testcase.printer')
 
-    local ok, err = pcall(function()
-        local printer = require('testcase.printer')
+    -- test that print all arguments without prefix
+    local p = printer.new()
+    argv = {}
+    p('format %q ', 'foo', ' a b ', 1, true, false)
+    assert.equal(argv, {
+        'format "foo"  a b 1truefalse',
+    })
 
-        -- test that print with format
-        local p = printer.new()
-        argv = {}
-        p('format value %q ', 'foo', 'and print following values as a string: ',
-          1, true, false)
-        assert.equal(argv, {
-            '',
-            'format value "foo" and print following values as a string: 1truefalse',
-        })
+    -- test that print all arguments with prefix
+    local prefix = 'hello > '
+    p = printer.new(prefix)
+    argv = {}
+    p('format %q ', 'foo', ' a b ', 1, true, false, 'with\n\nnewline\n')
+    assert.equal(argv, {
+        prefix,
+        'format "foo"  a b 1truefalsewith',
+        '\n',
+        prefix,
+        '',
+        '\n',
+        prefix,
+        'newline',
+        '\n',
+    })
 
-        -- test that arguments is not formatted if first argument is not format string
-        argv = {}
-        p('non-format string %%', 'hello', 'world')
-        assert.equal(argv, {
-            '',
-            'non-format string %%helloworld',
-        })
+    -- test that print all arguments and end with a suffix
+    local suffix = '[done]'
+    p = printer.new(prefix, suffix)
+    argv = {}
+    p('format %q ', 'foo', ' a b ', 1, true, false, 'with\n\nnewline\n')
+    assert.equal(argv, {
+        prefix,
+        'format "foo"  a b 1truefalsewith',
+        '\n',
+        prefix,
+        '',
+        '\n',
+        prefix,
+        'newline',
+        '\n',
+        suffix,
+    })
 
-        argv = {}
-        p(1, 'non-format string %s', 'hello')
-        assert.equal(argv, {
-            '',
-            '1non-format string %shello',
-        })
-
-        -- test that print each line with prefix, and suffix to last line
-        p = printer.new('=prefix=', '=suffix=')
-        argv = {}
-        p('foo\nbar\n\nbaz\n')
-        assert.equal(argv, {
-            '=prefix=',
-            'foo',
-            '\n',
-            '=prefix=',
-            'bar',
-            '\n',
-            '=prefix=',
-            '',
-            '\n',
-            '=prefix=',
-            'baz',
-            '\n',
-            '=suffix=',
-        })
-
-        -- test that throw error if invalid format string
-        local err = assert.throws(function()
-            p('non-format string %', 'hello')
-        end)
-        assert.match(err, "invalid .+ 'format'", false)
-
-        -- test that throw error with invalid arguments
-        for _, v in ipairs({
-            {
-                args = {
-                    true,
-                },
-                match = '#1 (nil or string',
-            },
-            {
-                args = {
-                    '',
-                    true,
-                },
-                match = '#2 (nil or string',
-            },
-        }) do
-            err = assert.throws(function()
-                printer.new(unpack(v.args))
-            end)
-            assert.match(err, v.match)
-        end
-    end)
+    -- test that print all arguments without formatting
+    local suffix = '[done]'
+    p = printer.new(prefix, suffix, false)
+    argv = {}
+    p('format %q ', 'foo', ' a b ', 1, true, false, 'with\n\nnewline\n')
+    assert.equal(argv, {
+        prefix,
+        'format %q foo a b 1truefalsewith',
+        '\n',
+        prefix,
+        '',
+        '\n',
+        prefix,
+        'newline',
+        '\n',
+        suffix,
+    })
 
     _G.io.stdout = stdout
-    assert(ok, err)
+    -- assert(ok, err)
 end
 
-test_printer()
+test_new()
+test_parse_format()
+test_vstringify()
+test_call_printline()
