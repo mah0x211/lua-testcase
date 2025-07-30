20cc05472bec4d1c04e5447db06b10f32852d3dd
mah0x211 <mah0x211@gmail.com>
2025-07-30 21:42:19 +0900

    Refactor code comments and restructure functions in bin/testcase.lua

--- bin/testcase.lua
+++ bin/testcase.lua
@@ -48,11 +48,59 @@ Options:
   --checkall    any file with a `.lua` extension will be evaluated as a test file
 ]]
 
+--- exit with code and message
+--- @param code number
+--- @param msg string?
+--- @param ... any
 local function exit(code, msg, ...)
     if msg then
         print(msg, ...)
     end
     osexit(code)
+end
+
+--- Check command line options and return options table
+--- @return table opts
+local function check_opts()
+    local opts = getopts(ARGV)
+    if opts['--help'] then
+        exit(0, USAGE)
+    elseif not opts[1] then
+        exit(-1, USAGE)
+    elseif opts['--coverage'] then
+        local ok, err = pcall(require, 'luacov')
+        if not ok then
+            exit(-1, 'failed to load luacov module: %s', err)
+        end
+    end
+
+    return opts
+end
+
+--- Get the pathname from command line arguments
+--- @return string pathname
+local function check_pathname(pathname)
+    -- confirm pathname
+    local path, err = realpath(pathname)
+    if err then
+        if err.type ~= ENOENT then
+            exit(-1, 'failed to resolve path %q: %s', pathname, err)
+        end
+        exit(-1, 'failed to resolve path %q', pathname)
+    end
+    return path
+end
+
+--- Get test files from the pathname
+--- @param opts table options
+--- @return table files
+local function get_files(opts)
+    local pathname = check_pathname(opts[1])
+    local files, err = getfiles(pathname, opts['--checkall'] and '.lua')
+    if err then
+        exit(-1, 'failed to get test files from %q: %s', pathname, err)
+    end
+    return files
 end
 
 --- loadfiles loads test files and runs it once for initialization
@@ -75,33 +123,8 @@ local function loadfiles(files)
 end
 
 do
-    local opts = getopts(ARGV)
-
-    if opts['--help'] then
-        exit(0, USAGE)
-    elseif not opts[1] then
-        exit(-1, USAGE)
-    elseif opts['--coverage'] then
-        local ok, err = pcall(require, 'luacov')
-        if not ok then
-            exit(-1, 'failed to load luacov module: %s', err)
-        end
-    end
-
-    -- confirm pathname
-    local pathname, err = realpath(opts[1])
-    if err then
-        if err.type ~= ENOENT then
-            exit(-1, 'failed to resolve path %q: %s', opts[1], err)
-        end
-        exit(-1, 'failed to resolve path %q', opts[1])
-    end
-
-    -- luacheck: ignore err
-    local files, err = getfiles(pathname, opts['--checkall'] and '.lua')
-    if err then
-        exit(-1, 'failed to get test files from %q: %s', pathname, err)
-    end
+    local opts = check_opts()
+    local files = get_files(opts)
 
     -- load test files
     runner.block()
